7c081e6ef77d4ba44005dafe14da11be1e5030b3
Jyotisaini1234 <jyoti.saini@surakshitnivesh.com>
2024-07-02 17:31:01 +0530

    54 - Section Summary

--- 03-pizza-menu/src/index.js
+++ 03-pizza-menu/src/index.js
@@ -59,40 +59,88 @@ function Header() {
   // const style = { color: "red", textAlign: "center", fontSize: "4rem" };
   const style = {};
   return (
-    <header className="header">
-      <h1 style={style}>Fast Pizza in the world</h1>
+    <header className="header footer">
+      <h1 style={style} className="header1">Fast Pizza in the world</h1>
+      <h2 className="our">Our Menu</h2>
+      <p className="p">Authantic Italic cuisine. 6 create dishes to choos from .All from our stone oven all organic and all delicious. </p>
     </header>
   );
 }
 function Menu() {
-  return (
+  // const pizzas=pizzaData;
+  // const numPizza=pizzas.length
+  return (  
     <main className="menu">
-      <h1>Our Menu</h1>
+{/* {numPizza > 0 ? (
+  <ul className="Pizzas">
+    {
+      pizzas.map((pizza)=>(
+        <Pizza pizzaObj={pizza} key={pizza.name} />
+      ))
+    }
+  </ul>
+  )  :null} */}
+
+   
+      <div className="first">
       <Pizza
         name="Pizza Spinaci"
         ingredients="Tomato, mozarella, ham, aragula, and burrata cheese"
         photoName="pizzas/spinaci.jpg"
         price={10}
-      />
-      <Pizza
-        name="Pizza  Prosciutto"
+      />    <Pizza
+      name="Pizza  Prosciutto"
+      ingredients="Tomato, mozarella, ham, aragula, and burrata cheese"
+      photoName="pizzas/prosciutto.jpg"
+      price={12}
+    />
+    
+    <Pizza
+        name="Pizza  Funghi"
         ingredients="Tomato, mozarella, ham, aragula, and burrata cheese"
-        photoName="pizzas/prosciutto.jpg"
-        price={12}
-      />
-    </main>
-  );
-}
-function Pizza(props) {
-  return (
-    <div className="pizza">
-      <img src={props.photoName} alt={props.name} />
-      <div>
-        <h3>{props.name}</h3>
-        <p>{props.ingredients}</p>
-        <span>{props.price + 2}</span>
-      </div>
+        photoName="pizzas/funghi.jpg"
+        price={14}
+        />
     </div>
+      <div className="second">
+
+  
+       <Pizza
+        name="Pizza  Focaccia"
+        ingredients="Tomato, mozarella, and pepperoni"
+        photoName="pizzas/focaccia.jpg"
+        price={22}
+      />
+       <Pizza
+        name="Pizza  Spainaci"
+        ingredients="Tomato, mozarella, ham, aragula, and burrata cheese"
+        photoName="pizzas/spinaci.jpg"
+        price={17}
+        
+      />
+        <Pizza 
+        name="Pizza  Salamino  "
+        ingredients="Tomato, mozarella, ham, aragula, and burrata cheese"
+        photoName="pizzas/salamino.jpg"
+        price={13}
+        
+      />
+      </div>
+    </main> 
+  )
+};
+function Pizza(pizzaObj) {
+  console.log(pizzaObj)
+  // if(pizzaObj.soldOut) return null;
+  return (
+    <li className={`pizza .${pizzaObj.soldOut ? "sold -out ":""}`}>
+      <img src={pizzaObj.photoName} alt={pizzaObj.name} />
+      <div>
+        <h3>{pizzaObj.name}</h3>
+        <p>{pizzaObj.ingredients}</p>
+        <span>{pizzaObj.soldOut ? "SOULD OUT" :pizzaObj.price }</span>
+      </div>
+    </li>
   );
 }
 
@@ -105,10 +153,29 @@ function Footer() {
   // if (hour >= openhour && hour <= closehour) alert("We're Currently Open");
   // else alert("Sorry We're Close");
   return (
+    
     <footer className="footer">
-      {new Date().toLocaleTimeString()}. We're currently open{" "}
+      {isOpen ? (
+        <Order closehour={closehour} openhour={openhour}/>
+      ):(
+        <p>
+          we'r happy to wecome you between  {openhour}:00 and {closehour}:00 
+        </p>
+      )}
     </footer>
   );
+}
+
+function Order({closehour,openhour}){
+  return(
+    <div className="order">
+      <p>
+We're open from {openhour}:00 to {closehour}:00 .Come visit us or order online 
+      </p>
+    <button className="btn">Order</button>
+
+    </div>
+  )
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
