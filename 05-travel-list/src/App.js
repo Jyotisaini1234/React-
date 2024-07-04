a5d86914f18471c0a4415534f7c615a3bd911223
Jyotisaini1234 <jyoti.saini@surakshitnivesh.com>
2024-07-04 16:33:30 +0530

    add new project

---
+++ 05-travel-list/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from '../public/img/far.jpeg'
+console.lof(logo)
+export default function App(){
+  return( <div>
+  <Logo/>
+  <Form/>
+  <PackingList/>
+  <State/>
+  </div>)}
+
+   function Logo(){
+    return <div>
+      {/* <img src={pizzaObj.photoName} alt={pizzaObj.name}/>
+      <img src={"../img/far.jpeg"}/> */}
+      <h1> win + Far Away</h1>
+    </div>
+   }
+function Form(){
+
+}
+function PackingList(){}
+function State(){}
