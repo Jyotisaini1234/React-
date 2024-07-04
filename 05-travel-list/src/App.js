145e8f1576893c5d72572887baa5a1a1e16215d4
Jyotisaini1234 <jyoti.saini@surakshitnivesh.com>
2024-07-04 17:28:11 +0530

    70 - Building the Layout

--- 05-travel-list/src/App.js
+++ 05-travel-list/src/App.js
@@ -1,8 +1,6 @@
-import React from 'react';
-import logo from '../public/img/far.jpeg'
-console.lof(logo)
+import './App.css';
 export default function App(){
-  return( <div>
+  return( <div className='app'>
   <Logo/>
   <Form/>
   <PackingList/>
@@ -11,13 +9,22 @@ export default function App(){
 
    function Logo(){
     return <div>
-      {/* <img src={pizzaObj.photoName} alt={pizzaObj.name}/>
-      <img src={"../img/far.jpeg"}/> */}
-      <h1> win + Far Away</h1>
+    
+      <h1 className="h1">  <div className="logo"></div> win + Far Away</h1>
+      
     </div>
    }
 function Form(){
+  return <div className='add-form'>
+<h3>What do you need for your Trip</h3>
+  </div>
 
 }
-function PackingList(){}
-function State(){}
+function PackingList(){
+  return <div className='list'>
+List
+  </div>
+}
+function State(){
+  return <footer className='stats'><em>You have X items on your list ,and you already packed X (X%)</em></footer>
+}
