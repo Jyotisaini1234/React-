792acbdf027490ec668f3340dd229996aea6ecfb
Jyotisaini1234 <jyoti.saini@surakshitnivesh.com>
2024-07-04 17:44:27 +0530

    71 - Rendering the Items List

--- 05-travel-list/src/App.js
+++ 05-travel-list/src/App.js
@@ -1,4 +1,14 @@
 import './App.css';
+
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 4, description: "Phone", quantity: 12, packed: true },
+  { id: 3, description: "Charger", quantity: 12, packed: false },
+
+
+];
+
 export default function App(){
   return( <div className='app'>
   <Logo/>
@@ -22,8 +32,15 @@ function Form(){
 }
 function PackingList(){
   return <div className='list'>
-List
+    <ul >
+{initialItems.map(item=><Item item={item}/>)}
+  </ul>
   </div>
+}
+function Item({item}){
+    return <li><span style={item.packed ? {textDecoration:"line-through"} : {}}>{item.quantity}{item.description}</span>
+    <button>&times;</button>
+    </li>
 }
 function State(){
   return <footer className='stats'><em>You have X items on your list ,and you already packed X (X%)</em></footer>
