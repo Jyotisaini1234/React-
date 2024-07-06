d7bbc429abca03a0badd948406cb0124db3d8257
Jyotisaini1234 <jyoti.saini@surakshitnivesh.com>
2024-07-06 16:24:55 +0530

    90 - The children Prop Making a Reusable Button

--- 04-steps/src/App-v1.js
+++ 04-steps/src/App-v1.js
@@ -37,27 +37,10 @@ return <div>
     </div>
     <p className="message">Step :{step}: {messages[step-1]}</p>
     <div className="buttons">
-
-<Button bgColor="#7950f2" 
-textColor="#fff"
-onClick={handlPrevious}
-text="Previous"
-/>
-<Button bgColor="#7950f2" 
-textColor="#fff"
-onClick={handlNext}
-text="Next"
-/>
-      {/* <button style={{backgroundColor:"#7950f2",color:"#fff"}} onClick={handlPrevious}>Previous</button> */}
-      {/* <button style={{backgroundColor:"#7950f2",color:"#fff"}} onClick={handlNext}>Next</button> */}
+      <button style={{backgroundColor:"#7950f2",color:"#fff"}} onClick={handlPrevious}>Previous</button>
+      <button style={{backgroundColor:"#7950f2",color:"#fff"}} onClick={handlNext}>Next</button>
     </div>
   </div>)
       }</div>
-  );
-}
-
-function Button({textColor,bgColor,onClick,text}){
-  return <button style={{backgroundColor:bgColor,color:textColor}} onClick={onClick}>{text}
-    
-  </button>
+  )
 }
